2df6f4e9bb20ebd9052efb4c0ebf5e00eb37b557
fitma <fitgrisha@gmail.com>
2021-02-20 16:07:54 +0300

    add source

--- pages/index.vue
+++ pages/index.vue
@@ -35,6 +35,78 @@
         </a>
       </div>
     </div>
+    <div>
+      <Logo />
+      <h1 class="title">
+        nuxt-express
+      </h1>
+      <div>
+        {{ test }}
+        <div class="links">
+          <a
+              href="/users"
+              class="button--green"
+          >
+            Users List
+          </a>
+        </div>
+      </div>
+      <div class="links">
+        <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+
+    <div>
+      <Logo />
+      <h1 class="title">
+        nuxt-express
+      </h1>
+      <div>
+        {{ test }}
+        <div class="links">
+          <a
+              href="/users"
+              class="button--green"
+          >
+            Users List
+          </a>
+        </div>
+      </div>
+      <div class="links">
+        <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+
   </div>
 </template>
 
